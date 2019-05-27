d8c4c318d1b47a4dec3b582b0845a76671642c95
Victor Chong <victor.chong@linaro.org>
2019-05-27 23:46:17 +0900

    master_crypto: improve secret key creation process
    TEE_OpenPersistentObject() in TA_create_secret_key() is unnecessary.
    It'll just be closed (TEE_CloseObject()) again.
    Just create the key and be done with.
    
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- keymaster/ta/master_crypto.c
+++ keymaster/ta/master_crypto.c
@@ -89,16 +89,13 @@ close:
 
 TEE_Result TA_create_secret_key(void)
 {
-	TEE_Result res;
+	TEE_Result res = TEE_SUCCESS;
 	TEE_ObjectHandle object = TEE_HANDLE_NULL;
 	uint8_t keyData[KEY_LENGTH];
+	static bool created = false;
 
 	DMSG("%s %d", __func__, __LINE__);
-	res = TEE_OpenPersistentObject(TEE_STORAGE_PRIVATE,
-				objID, sizeof(objID),
-				TEE_DATA_FLAG_ACCESS_READ, &object);
-
-	if (res == TEE_ERROR_ITEM_NOT_FOUND) {
+	if (!created) {
 		//No such key, create it
 		TEE_GenerateRandom(keyData, sizeof(keyData));
 		TEE_GenerateRandom((void *)iv, sizeof(iv));
@@ -125,17 +122,16 @@ TEE_Result TA_create_secret_key(void)
 			goto error;
 		}
 
-error:
-		(res == TEE_SUCCESS) ?
-				TEE_CloseObject(object) :
-				TEE_CloseAndDeletePersistentObject(object);
-
-	} else if (res == TEE_SUCCESS) {
-		//Key already exits
 		TEE_CloseObject(object);
-	} else {
-		//Something wrong...
-		EMSG("Failed to open secret key, res=%x", res);
+		created = true;
+
+error:
+		if (res != TEE_SUCCESS) {
+			//Something wrong...
+			EMSG("Failed to create secret key, res=%x", res);
+			TEE_CloseAndDeletePersistentObject(object);
+		}
+
 	}
 
 	return res;
