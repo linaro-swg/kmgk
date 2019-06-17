fe3f298e37d95da26c7bf6f4dd1b4a399b25cdec
Olivier Masse <olivier.masse@nxp.com>
2019-06-17 17:03:43 +0200

    Gatekeeper TA: Stack size 16Ko

--- gatekeeper/ta/user_ta_header_defines.h
+++ gatekeeper/ta/user_ta_header_defines.h
@@ -27,7 +27,7 @@
 #define TA_UUID TA_GATEKEEPER_UUID
 
 #define TA_FLAGS                    (TA_FLAG_MULTI_SESSION | TA_FLAG_EXEC_DDR)
-#define TA_STACK_SIZE               (2 * 1024)
+#define TA_STACK_SIZE               (16 * 1024)
 #define TA_DATA_SIZE                (32 * 1024)
 
 #define TA_CURRENT_TA_EXT_PROPERTIES \
