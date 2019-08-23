da75fddd032f7bc23e1cc67f2723ca455b274735
Victor Chong <victor.chong@linaro.org>
2019-08-23 17:45:03 +0100

    ta: update flags
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- gatekeeper/ta/user_ta_header_defines.h
+++ gatekeeper/ta/user_ta_header_defines.h
@@ -26,9 +26,10 @@
 
 #define TA_UUID TA_GATEKEEPER_UUID
 
-#define TA_FLAGS                    (TA_FLAG_MULTI_SESSION | TA_FLAG_EXEC_DDR)
-#define TA_STACK_SIZE               (16 * 1024)
-#define TA_DATA_SIZE                (32 * 1024)
+#define TA_FLAGS	(TA_FLAG_USER_MODE | TA_FLAG_EXEC_DDR | \
+			TA_FLAG_SINGLE_INSTANCE | TA_FLAG_MULTI_SESSION)
+#define TA_STACK_SIZE	(16 * 1024)
+#define TA_DATA_SIZE	(32 * 1024)
 
 #define TA_CURRENT_TA_EXT_PROPERTIES \
     { "gp.ta.description", USER_TA_PROP_TYPE_STRING, \
