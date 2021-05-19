8cb9b43420a50b7b6a1ad59a4d5d9a5058b7a019
Victor Chong <victor.chong@linaro.org>
2021-05-19 10:44:59 +0100

    Revert "km: ta: replace SIZE_LENGTH with SIZE_LENGTH_AKMS"
    This reverts commit 91aba4ca7f3d01115314733e399a348b5b1c9ab4.
    
    This causes errors in VtsHalKeymasterV3_0TargetTest.

--- keymaster/ta/parsel.c
+++ keymaster/ta/parsel.c
@@ -345,18 +345,13 @@ int TA_deserialize_param_set(uint8_t *in, uint8_t *end,
 	}
 	if (p == KM_NULL)
 		return in - start;
-	if (TA_is_out_of_bounds(in, end, SIZE_LENGTH_AKMS)) {
+	if (TA_is_out_of_bounds(in, end, SIZE_LENGTH)) {
 		EMSG("Out of input array bounds on deserialization");
 		*res = KM_ERROR_INSUFFICIENT_BUFFER_SPACE;
 		return in - start;
 	}
 	TEE_MemMove(&params->length, in, sizeof(params->length));
-	EMSG("####################");
-	EMSG("sizeof(size_t) = %zu", sizeof(size_t));
-	EMSG("in = 0x%" PRIXPTR, (uintptr_t)in);
-	EMSG("####################");
-	in += SIZE_LENGTH_AKMS;
-	EMSG("in = 0x%" PRIXPTR, (uintptr_t)in);
+	in += SIZE_LENGTH;
 
 	DMSG("params->length:%zu", params->length);
 	if (params->length > MAX_ENFORCED_PARAMS_COUNT) {
@@ -763,12 +758,7 @@ int TA_serialize_characteristics(uint8_t *out, uint8_t *out_end,
 	}
 	TEE_MemMove(out, &characteristics->hw_enforced.length,
 		    sizeof(characteristics->hw_enforced.length));
-	EMSG("####################");
-	EMSG("sizeof(size_t) = %zu", sizeof(size_t));
-	EMSG("out = 0x%" PRIXPTR, (uintptr_t)out);
-	EMSG("####################");
-	out += SIZE_LENGTH_AKMS;
-	EMSG("out = 0x%" PRIXPTR, (uintptr_t)out);
+	out += SIZE_LENGTH;
 	for (size_t i = 0; i < characteristics->hw_enforced.length; i++) {
 		if (TA_is_out_of_bounds(out, out_end,
 			SIZE_OF_ITEM(characteristics->hw_enforced.params))) {
@@ -803,12 +793,7 @@ int TA_serialize_characteristics(uint8_t *out, uint8_t *out_end,
 	}
 	TEE_MemMove(out, &characteristics->sw_enforced.length,
 		    sizeof(characteristics->sw_enforced.length));
-	EMSG("####################");
-	EMSG("sizeof(size_t) = %zu", sizeof(size_t));
-	EMSG("out = 0x%" PRIXPTR, (uintptr_t)out);
-	EMSG("####################");
-	out += SIZE_LENGTH_AKMS;
-	EMSG("out = 0x%" PRIXPTR, (uintptr_t)out);
+	out += SIZE_LENGTH;
 	for (size_t i = 0; i < characteristics->sw_enforced.length; i++) {
 		if (TA_is_out_of_bounds(out, out_end,
 			SIZE_OF_ITEM(characteristics->sw_enforced.params))) {
